82c06288b53583a4aff40a0d13dec364aa2b095b
Lubna Fathima <lubnafathima275@gmail.com>
2024-10-21 15:52:10 +0530

    Feat: Added selector

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -23,6 +23,7 @@ const Home = () => {
   const [appearance, setAppearance] = useState("light");
   const [cornerRadius, setCornerRadius] = useState("rounded-none");
   const textColor = "#" + brandColor;
+  const selectionColor = appearance === "light" ? "#fc1" : "#ffd45e";
 
   const renderComponent = () => {
     switch (activeTab) {
@@ -126,7 +127,7 @@ const Home = () => {
   };
 
   const lightGradients = {
-    "db2777":
+    db2777:
       "radial-gradient(50% 50% at 50% 100%, #E1B5C9FF 0%, #073AFF00 100%),radial-gradient(113% 91% at 17% -2%, #2000240d 1%, #FF000000 99%),radial-gradient(142% 91% at 83% 7%, #2000240d 1%, #FF000000 99%),radial-gradient(142% 91% at -6% 74%, #2000240d 1%, #FF000000 99%),radial-gradient(142% 91% at 111% 84%, #2000240d 0%, #FFFFFFFF 100%)",
     "9333ea":
       "radial-gradient(50% 50% at 50% 100%, #D1BEE1FF 0%, #073AFF00 100%),radial-gradient(113% 91% at 17% -2%, #2000240d 1%, #FF000000 99%),radial-gradient(142% 91% at 83% 7%, #2000240d 1%, #FF000000 99%),radial-gradient(142% 91% at -6% 74%, #2000240d 1%, #FF000000 99%),radial-gradient(142% 91% at 111% 84%, #2000240d 0%, #FFFFFFFF 100%)",
@@ -137,7 +138,7 @@ const Home = () => {
   };
 
   const darkGradients = {
-    "db2777":
+    db2777:
       "radial-gradient(20% 20% at 50% 100%, #2B2629FF 0%, #073AFF00 100%),radial-gradient(113% 91% at 17% -2%, #240029 1%, #FF000000 99%),radial-gradient(142% 91% at 83% 7%, #240029 1%, #FF000000 99%),radial-gradient(142% 91% at -6% 74%, #240029 1%, #FF000000 99%),radial-gradient(142% 91% at 111% 84%, #240029 0%, #FF0000FF 100%)",
     "9333ea":
       "radial-gradient(20% 20% at 50% 100%, #39244DFF 0%, #073AFF00 100%),radial-gradient(113% 91% at 17% -2%, #240029 1%, #FF000000 99%),radial-gradient(142% 91% at 83% 7%, #240029 1%, #FF000000 99%),radial-gradient(142% 91% at -6% 74%, #240029 1%, #FF000000 99%),radial-gradient(142% 91% at 111% 84%, #240029 0%, #9333ea 99%)",
@@ -150,24 +151,6 @@ const Home = () => {
   const currentGradients =
     appearance === "light" ? lightGradients : darkGradients;
 
-  const light = {
-    "db2777": "l1",
-    "9333ea": "l2",
-    "2563eb": "l3",
-    "16a34a": "l4",
-  };
-
-  const dark = {
-    "db2777": "d1",
-    "9333ea": "d2",
-    "2563eb": "d3",
-    "16a34a": "d4",
-  };
-
-  const current = appearance === "light" ? light : dark;
-
-  console.log(current[brandColor]);
-
   return (
     <div
       className="w-full min-h-screen text-black flex flex-col py-20 px-4 md:px-0"
@@ -175,6 +158,14 @@ const Home = () => {
         background: currentGradients[brandColor],
       }}
     >
+      <style>
+        {`
+          ::selection {
+            background: ${selectionColor};
+            color: inherit;
+          }
+        `}
+      </style>
       <Header
         activeTab={activeTab}
         setActiveTab={setActiveTab}
